403df4dc4e7ea21faaa98632ab26543d1a3c8d18
oebear <ohtoautio@gmail.com>
2023-05-01 19:46:57 +0300

    lots of progress done

--- Polarhood/Form1.Designer.cs
+++ Polarhood/Form1.Designer.cs
@@ -37,16 +37,32 @@
             this.label1 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.label3 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -122,6 +138,16 @@
             // 
             // panel3
             // 
+            this.panel3.Controls.Add(this.numericUpDown1);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.panel5);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Controls.Add(this.label10);
+            this.panel3.Controls.Add(this.label9);
+            this.panel3.Controls.Add(this.label8);
+            this.panel3.Controls.Add(this.label7);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Controls.Add(this.label5);
             this.panel3.Controls.Add(this.label4);
             this.panel3.Controls.Add(this.button1);
             this.panel3.Controls.Add(this.label3);
@@ -131,6 +157,115 @@
             this.panel3.Name = "panel3";
             this.panel3.Size = new System.Drawing.Size(598, 450);
             this.panel3.TabIndex = 2;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.textBox2);
+            this.panel5.Location = new System.Drawing.Point(20, 183);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(259, 255);
+            this.panel5.TabIndex = 11;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Location = new System.Drawing.Point(3, 12);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(253, 222);
+            this.textBox2.TabIndex = 0;
+            // 
+            // panel4
+            // 
+            this.panel4.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel4.AutoScroll = true;
+            this.panel4.Controls.Add(this.label12);
+            this.panel4.Controls.Add(this.label11);
+            this.panel4.Location = new System.Drawing.Point(386, 73);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(200, 365);
+            this.panel4.TabIndex = 10;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(30, 310);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(46, 16);
+            this.label12.TabIndex = 1;
+            this.label12.Text = "stock2";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(13, 20);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(39, 16);
+            this.label11.TabIndex = 0;
+            this.label11.Text = "stock";
+            // 
+            // label10
+            // 
+            this.label10.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.Location = new System.Drawing.Point(474, 9);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(100, 22);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "My stocks";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(16, 141);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(130, 22);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Related News";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(101, 82);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(80, 22);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Example";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(102, 104);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(20, 22);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(36, 104);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(60, 22);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Price";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(36, 82);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(50, 22);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Name";
             // 
             // label4
             // 
@@ -143,20 +278,22 @@
             // 
             // button1
             // 
+            this.button1.Font = new System.Drawing.Font("Consolas", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.button1.Location = new System.Drawing.Point(187, 29);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
-            this.button1.Text = "Check";
+            this.button1.Text = "Search";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(33, 33);
+            this.label3.Font = new System.Drawing.Font("Consolas", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(16, 30);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 16);
+            this.label3.Size = new System.Drawing.Size(60, 22);
             this.label3.TabIndex = 1;
             this.label3.Text = "Stock";
             // 
@@ -169,18 +306,36 @@
             this.textBox1.TabIndex = 0;
             this.textBox1.Text = "Ticker";
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(218, 104);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Buy";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(171, 104);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(41, 22);
+            this.numericUpDown1.TabIndex = 13;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.Window;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Polarhood";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
@@ -190,6 +345,11 @@
             this.panel2.PerformLayout();
             this.panel3.ResumeLayout(false);
             this.panel3.PerformLayout();
+            this.panel5.ResumeLayout(false);
+            this.panel5.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -208,6 +368,19 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Polarhood/Form1.cs
+++ Polarhood/Form1.cs
@@ -1,15 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Reflection;
-using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Globalization;
+
 
 namespace Polarhood
 {
@@ -38,13 +35,118 @@ namespace Polarhood
         private void button1_Click(object sender, EventArgs e)
         {
             string ticker1 = textBox1.Text;
-            string path1 = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            using (StreamWriter writer = new StreamWriter(path1 + "\\stockprice.txt."))
-            {
-                writer.WriteLine(ticker1+ ":NASDAQ");
+            string pythonFilePath = "C:\\Users\\ohtoa\\vscodeapps\\price1.py";
+            string arguments = ticker1; // replace with your own arguments
 
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "python"; // set the filename to the Python interpreter
+            startInfo.Arguments = $"{pythonFilePath} {arguments}"; // set the arguments for the Python script
+            startInfo.UseShellExecute = false;
+            startInfo.RedirectStandardOutput = true;
+            Process process = new Process();
+            process.StartInfo = startInfo;
+            process.Start();
+            List<string> outputLines = new List<string>();
+            string line;
+            while ((line = process.StandardOutput.ReadLine()) != null)
+            {
+                outputLines.Add(line);
+            }
+            process.WaitForExit();
+            label8.Text = outputLines[1];
+            label7.Text = outputLines[0];
+            outputLines.RemoveRange(0, 2);
+            foreach(string a in outputLines) {
+                textBox2.Text += a;
+            
+            }
+
+
+            
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string ticker = textBox1.Text;  
+            int ammount = Decimal.ToInt32(numericUpDown1.Value);
+            string name = label8.Text;
+            decimal price = decimal.Parse(label7.Text, CultureInfo.InvariantCulture);
+            string path1 = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\data1.txt";
+            using (FileStream fs = new FileStream(path1, FileMode.Append))
+            {
+                using (StreamWriter w = new StreamWriter(fs))
+                {
+                    w.WriteLine(ticker);
+                    w.WriteLine(name);
+                    w.WriteLine(price);
+                    w.WriteLine(ammount);
+                    w.Close();
+                }
+                fs.Close();
+            }
+
+
+            Button sell1 = new Button();
+            sell1.Text = "Sell all";
+            sell1.Location = new Point(27, 27);
+            panel4.Controls.Add(sell1);
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // values
+            int ticker1 = 0;
+            int distance1 = 13;
+            int lines1 = 0;
+            // path of the txt file where all your stocks and their values and stored
+            string path1 = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location) + "\\data1.txt";
+            // checking if the file exist and if it does it displays its values and if not it creates it
+            if (File.Exists(path1))
+            {
+                string[] stockdata = File.ReadAllLines(path1);
+                foreach (string a in stockdata)
+                {
+                    lines1 += 1;
+
+                }
+                int stockammount = lines1 / 4;
+                for (int i = stockammount; i > 0; i--)
+                {               
+                    // creating buttons and labels to my stocks section
+                    Label ticker= new Label();
+                    ticker.Text = stockdata[ticker1];
+                    ticker.Location = new Point(13, distance1);
+                    panel4.Controls.Add(ticker);
+
+                    Button sell2 = new Button();
+                    sell2.Text = "Sell";
+                    sell2.Location = new Point(109, distance1);
+                    sell2.Click += new EventHandler(sell2_Click);    
+                    panel4.Controls.Add(sell2);
+
+                    distance1 += 26;
+                    ticker1 += 4;
+                }
+            }
+            else
+            {
+                using (FileStream fs = File.Create(path1))
+                {
+                    
+                }
             }
             
+
+        }
+
+        private void sell2_Click(object sender, EventArgs e)
+        {
+            // This code will execute when any of the sell buttons are clicked.
+            Console.WriteLine();
+
         }
     }
 }
